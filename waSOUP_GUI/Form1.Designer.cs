8f3aa47f85f3472ddf058b70d639c55515f2b85c
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-05-25 21:10:31 +0200

    first commit

---
+++ waSOUP_GUI/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace waSOUP_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listTracks = new System.Windows.Forms.ListView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnPlayPause = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listTracks
+            // 
+            this.listTracks.Location = new System.Drawing.Point(12, 89);
+            this.listTracks.Name = "listTracks";
+            this.listTracks.Size = new System.Drawing.Size(272, 505);
+            this.listTracks.TabIndex = 0;
+            this.listTracks.UseCompatibleStateImageBehavior = false;
+            this.listTracks.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listTracks_MouseDoubleClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(308, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(337, 170);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(308, 254);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(337, 170);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "REMOVE";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(308, 499);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(337, 170);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "EDIT";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnPlayPause
+            // 
+            this.btnPlayPause.Location = new System.Drawing.Point(12, 600);
+            this.btnPlayPause.Name = "btnPlayPause";
+            this.btnPlayPause.Size = new System.Drawing.Size(272, 69);
+            this.btnPlayPause.TabIndex = 4;
+            this.btnPlayPause.Text = "PLAY/PAUSE";
+            this.btnPlayPause.UseVisualStyleBackColor = true;
+            this.btnPlayPause.Click += new System.EventHandler(this.btnPlayPause_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(13, 39);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(271, 20);
+            this.txtSearch.TabIndex = 5;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnPlayPause);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listTracks);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listTracks;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnPlayPause;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ waSOUP_GUI/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace waSOUP_GUI
+{
+    public partial class Form1 : Form
+    {
+        private bool pause = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.listTracks.Items.AddRange(new ListViewItem[]{
+                new ListViewItem("test", 0)
+            });
+        }
+
+        private void listTracks_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine(this.listTracks.SelectedItems[0].Text);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("ADD");
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if(this.listTracks.SelectedItems.Count == 0)
+                Console.WriteLine("REMOVE ");
+            else
+                Console.WriteLine("REMOVE " + this.listTracks.SelectedItems[0].Text);
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (this.listTracks.SelectedItems.Count == 0)
+                Console.WriteLine("EDIT ");
+            else
+                Console.WriteLine("EDIT " + this.listTracks.SelectedItems[0].Text);
+        }
+
+        private void btnPlayPause_Click(object sender, EventArgs e)
+        {
+            this.pause = !this.pause;
+            if (this.pause)
+                Console.WriteLine("PLAY");
+            else
+                Console.WriteLine("PAUSE");
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("text changed");
+        }
+    }
+}
